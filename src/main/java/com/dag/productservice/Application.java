efa7b498d7afed091ff37e446c26bc7c90c3018a
Tusharvatsa02 <83219333+Tusharvatsa02@users.noreply.github.com>
2024-03-21 11:53:04 +0530

    Application class change

--- src/main/java/com/dag/productservice/Application.java
+++ src/main/java/com/dag/productservice/Application.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @OpenAPI30
-public class ProductserviceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductserviceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
